4e77f28cf11cfc81594a60dee9afe308300a72c8
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:50:31 -0800

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+'''a=10
+b=20
+print(a+b)
+print("a"+"b")
+print("3"+"3")'''
+#======================================================
+
+a= input("Enter first number:")
+b= input("Enter second number:")
+print(a+b)
