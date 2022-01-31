5eec5e1bf7a19d453fb753ff9685c03cfc7fc55d
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:53:47 -0800

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,56 @@
+mytuple = ('prashant','Ashish','Rahul','ankit','komal','ankush','Ashish',77,'sandip',60.52,77)
+print(mytuple)
+'''print(type(mytuple))
+print(mytuple[0])
+print(mytuple[1])
+print(mytuple[2])
+print(mytuple[-1])
+print(mytuple[2:5])
+print(mytuple[:5])
+print(mytuple[1:])
+print(len(mytuple))
+print(mytuple.count(77))'''
+#===========================================================================
+
+'''mytuple=(['prashant','jha'],['85.56'],[440022,'yyy'])
+print('example of multidimensional tuple:')
+print(mytuple)
+print(mytuple[0][0])
+print(mytuple[0][1])
+print(mytuple[1][0])
+print(mytuple[2][0])
+print(mytuple[2][1])'''
+#===========================================================================
+
+'''mytuple1=('prashant','jha')
+print(mytuple1*2)
+mytuple2=(50,25.50)
+print(mytuple1+mytuple2)'''
+#=========================================================================
+
+'''name= 'hepl4code containing 5000 plus programs'
+mytuple= name.split()
+print(mytuple)
+print(name)'''
+#=========================================================================
+
+'''mytuple= (44,22,77,0,9,88)
+print(sorted(mytuple))'''
+#==========================================================================
+
+'''mytuple= (44,22,77,0,9,88)
+newtuple= mytuple
+print(id(mytuple))
+print(id(newtuple))'''
+#==========================================================================
+
+'''mytuple= (1,2,3,4,5)
+print(max(mytuple))'''
+#=========================================================================
+
+'''mytuple= (1,2,3,4,5)
+print(min(mytuple))'''
+#==========================================================================
+
+'''mytuple= (11,18,19,21,46)
+print(sum(mytuple))'''
