121513b5f660cef3836af1dd2e2518caa102f5fb
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:15:24 -0800

    Add files via upload

---
+++ brand.py
@@ -0,0 +1,9 @@
+brand= input("Enter your coldrink name:")
+if brand=='pepsi' or brand=='PEPSI':
+    print("Swag")
+elif brand=='dew' or brand=='DEW':
+    print("Dar ke age jeet hai")
+elif brand=='thumsup' or brand=='THUMSUP':
+    print("Taste the thunder")
+else:
+    print("Go with your brand")
