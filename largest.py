2156771498c1c8de5f182a1f314b92132de0b87b
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:34:59 -0800

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,15 @@
+n1= int(input("Enter first number:"))
+n2= int(input("Enter second number:"))
+n3= int(input("Enter third number:"))
+n4= int(input("Enter fourth number:"))
+n5= int(input("Enter fifth number:"))
+max=n1
+if max<n2:
+    max=n2
+if max<n3:
+    max=n3
+if max<n4:
+    max=n4
+if max<n5:
+    max=n5
+print("maximum number is",max)
