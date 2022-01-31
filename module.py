565aed72c3c5eaf741b0c76dd87dee3a0e4df6dc
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:17:17 -0800

    Add files via upload

---
+++ module.py
@@ -0,0 +1,57 @@
+'''import math
+print(math.sqrt(16))
+print(math.pi)'''
+#==========================================================================
+
+'''import math as m
+print(m.sqrt(20))
+print(m.pi)'''
+#==========================================================================
+
+'''from math import*
+print(int(sqrt(4)))
+print(ceil(10.1))
+print(floor(10.1))
+print(fabs(-10.6))
+print(fabs(10.6))'''
+#==========================================================================
+
+'''from random import*
+for i in range(5):
+    print(random())'''
+#==========================================================================
+
+'''from random import*
+for i in range(10):
+    print(randint(1,100000))'''
+#==========================================================================
+
+'''from random import*
+for i in range(10):
+    print(uniform(1,10))'''
+#==========================================================================
+
+'''from random import*
+list= ["prashant","rahul","ashish","sandip","sunil","nikhil"]
+for i in range(10):
+    print(choice(list))'''
+#==========================================================================
+
+'''from random import*
+tuple= ("prashant","rahul","ashish","sandip","sunil","nikhil")
+for i in range(10):
+    print(choice(tuple))'''
+#==========================================================================
+
+'''def call():
+    if __name__=='__main__':
+        print("This is executed as a individual program")
+    else:
+        print("This is executed as a module from some other program")
+call()'''
+#==========================================================================
+
+'''from random import*
+print(randint(1,100000))'''
+#==========================================================================
+
