31ac636108dfc27f2fb9efdf476170f6d9486acd
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:47:38 -0800

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,18 @@
+'''age=20
+name= 'Vaibhavi'
+perc= 88.60
+status= True
+print("Age=",age)
+print("Name=",name)
+print("Percentage=",perc)
+print("True/False=",status)'''
+#================================================
+
+'''age=20
+name= 'Vaibhavi'
+perc= 88.60
+status= True
+print("Age=",type(age))
+print("Name=",type(name))
+print("Percentage=",type(perc))
+print("True/False=",type(status))'''
