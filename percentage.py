b6dbd5da34f68dfe880cdede1f7a3f9e1e3d0c2e
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:43:39 -0800

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,9 @@
+p1= int(input("Enter percentage:"))
+if p1>=40 and p1<60:
+    print("Your grade is C")
+elif p1>=60 and p1<=80:
+    print("Your grade is B")
+elif p1>=80 and p1<=100:
+    print("Your grade is A")
+else:
+    print("Fail")
