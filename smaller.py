a95ce45ea50a9ee3f80baed1dc694daae56d3d10
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:49:10 -0800

    Add files via upload

---
+++ smaller.py
@@ -0,0 +1,13 @@
+a= int(input("Enter first no:"))
+b= int(input("Enter second no:"))
+c= int(input("Enter third no:"))
+if a<b:
+    if a<c:
+        print("A is smaller")
+    else:
+        print("C is smaller")
+else:
+    if b<c:
+        print("B is smaller")
+    else:
+        print("C is smaller")
