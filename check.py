a7b45a5bdf9b985b71bddd676301f1038dda148e
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:23:59 -0800

    Add files via upload

---
+++ check.py
@@ -0,0 +1,8 @@
+a= int(input("enter any number:"))
+if a>0:
+    print("Your entered number is positive")
+if a<0:
+    print("Your entered number is negative")
+if a==0:
+    print("Your entered number is zero")
+print("Operation done")
