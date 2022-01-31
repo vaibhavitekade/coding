7c2e4a4e4311381f05066480272ddd7a82808541
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:49:49 -0800

    Add files via upload

---
+++ smallest.py
@@ -0,0 +1,15 @@
+n1= int(input("Enter first number:"))
+n2= int(input("Enter second number:"))
+n3= int(input("Enter third number:"))
+n4= int(input("Enter fourth number:"))
+n5= int(input("Enter fifth number:"))
+min=n1
+if min>n2:
+    min=n2
+if min>n3:
+    min=n3
+if min>n4:
+    min=n4
+if min>n5:
+    min=n5
+print("Minimum number is",min)
