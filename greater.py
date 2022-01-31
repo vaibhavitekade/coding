0904953196bd5466fba33fbaea653799a9262434
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:33:37 -0800

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,13 @@
+a= int(input("Enter first no:"))
+b= int(input("Enter second no:"))
+c= int(input("Enter third no:"))
+if a>b:
+    if a>c:
+        print("A is greater")
+    else:
+        print("C is greater")
+else:
+    if b>c:
+        print("B is greater")
+    else:
+        print("C is greater")
