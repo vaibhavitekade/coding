c17c387d1888349bed00c6160df077db13b30af3
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:38:45 -0800

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,11 @@
+#WAP for taking 5 paper marks and calculate total and percentage
+
+p1=81
+p2=78
+p3=88
+p4=91
+p5=95
+total=p1+p2+p3+p4+p5
+percentage=(total/5)
+print("Total marks are:",total)
+print("Percentage is:",percentage)
