1ffc897b0997e99516f786f1dbbf72573356c2e1
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:27:54 -0800

    Add files via upload

---
+++ control3.py
@@ -0,0 +1,11 @@
+'''list= [1,2,3,4,5,6,7,8,9,10]
+sum=0
+for x in list:
+    sum= sum+x
+print("The sum =",sum)'''
+#======================================================
+
+'''sum=0
+for x in range(1,15):
+    sum= sum+x
+print("The sum =",sum)'''
