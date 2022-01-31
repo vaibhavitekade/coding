16b2583022387ccf4f47a6ef5c1b3cdd5af548cf
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:38:06 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,108 @@
+'''mylist=['prashant','rahul','yash','kartik',23,'sandip',60.52]
+print(mylist)
+print(type(mylist))
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[-1])
+print(mylist[-4:-1])
+print(mylist[2:5])
+print(mylist[2:4])
+print(mylist[:5])
+print(mylist[1:])
+print(len(mylist))'''
+#================================================================
+
+'''mylist[2]='suraj'
+print(mylist)'''
+#================================================================
+
+'''mylist.append('pranay')
+print(mylist)'''
+#===============================================================
+
+'''mylist.insert(3,'vaibhavi')
+print(mylist)'''
+#===============================================================
+
+'''mylist.remove('sandip')
+print(mylist)'''
+#===============================================================
+
+'''newlist= mylist.copy()
+print(newlist)'''
+#==============================================================
+
+'''mylist= [['prashant','jha'],['85.56'],[440022,'yyy']]
+print('example of multidimensional list:')
+print(mylist)
+print(mylist[0][0])
+print(mylist[0][1])
+print(mylist[1][0])
+print(mylist[2][0])
+print(mylist[2][1])'''
+#=============================================================
+
+'''list1= ['prashant','jha']
+print(list1*2)
+list2= [50,25.50]
+print(list1+list2)'''
+#=============================================================
+
+'''list2= [50,25.50,'prashant']
+del list2[0]
+del list2
+print(list2)'''
+#============================================================
+
+'''list2= [50,25.50,'prashant']
+print(list2)
+list2.clear()
+print(list2)'''
+#============================================================
+
+'''name= 'prashant'
+print(name)
+myname= list(name)
+print(myname)'''
+#============================================================
+
+'''name= "help4code containing 5000 plus programs"
+mylist= name.split()
+print(mylist)
+print(name)'''
+#===========================================================
+
+'''mylist= [40,30,20,10]
+mylist.reverse()
+print(mylist)'''
+#===========================================================
+
+'''mylist= [44,22,77,0,9,88]
+mylist.sort()
+print(mylist)'''
+#===========================================================
+
+'''mylist= [44,22,77,0,9,88]
+newlist=mylist
+print(id(mylist))
+print(id(newlist))'''
+#===========================================================
+
+'''mylist= [44,22,77,0,9,88]
+newlist=mylist
+newlist[3]=2
+print(mylist)'''
+#============================================================
+
+'''mylist1= ['sunil',3,67]
+mylist.extend(mylist1)
+print(mylist)
+print(mylist1.index(3))'''
+#============================================================
+
+'''mylist2= [456,700,200,45,87,200]
+print(max(mylist2))
+print(min(mylist2))
+print(mylist2.count(200))'''
+#==============================================================
