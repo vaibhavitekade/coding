05b95bd751ace269cb51c69806dfae74d67e3a9e
vaibhavitekade <98414830+vaibhavitekade@users.noreply.github.com>
2022-01-30 21:29:28 -0800

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,54 @@
+# int() used to convert
+'''print(int(3.14))
+#print(int(10+5j))
+print(int(True))
+print(int(False))
+#print(int("4.22"))
+print(int("4"))
+#print(int("vaibhavi"))'''
+#===========================================================
+
+# float() use to convert
+'''print(float(3))
+#print(float(50+2j))
+print(float(True))
+print(float(False))
+print(float("4.22"))
+print(float("4"))
+#print(float("name"))'''
+#==========================================================
+
+# complex() used to convert
+'''print(complex(3))
+print(complex(12.5))
+print(complex(True))
+print(complex(False))
+print(complex("5"))
+print(complex("5.6"))
+#print(complex(" "))
+#print(complex("name"))
+print(complex(5,-3))
+print(complex(True,False))'''
+#=========================================================
+
+# bool() is used to convert
+'''print(bool(0))
+print(bool(15))
+print(bool(3.14))
+print(bool(0.0))
+print(bool(1+2j))
+print(bool(0+0j))
+print(bool(-1))
+print(bool(False))
+print(bool(True))
+#print(bool(" "))
+#print(bool("ggg"))'''
+#=========================================================
+
+# str() is used to convert 
+'''print(str(100))
+print(str(3.14))
+print(str(True))
+print(str(False))
+print(str(5+2j))
+print(str('A'))'''
